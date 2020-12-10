1ab327da4e946bd90bdcf774bdcd518d7d06cf63
jorgeus16 <jorgechucena@gmail.com>
2020-12-10 12:33:55 +0000

    change of project structure to good practices

--- backend/source/routes/deliveries.js
+++ backend/source/routes/deliveries.js
@@ -1,82 +1,26 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var dataStore = require('nedb');
+const express = require("express");
+const { validateDeliveryData } = require("../../utils/validators");
 
-const app = express();
-const expressSwagger = require('express-swagger-generator')(app);
-
-var port = (process.env.PORT || 3000);
-var BASE_API_PATH = "/api/v1";
-var DB_FILE_NAME = __dirname + "/contacts.json";
-
-let options = {
-    swaggerDefinition: {
-        info: {
-            description: 'This is a sample server',
-            title: 'Swagger',
-            version: '1.0.0',
-        },
-        host: 'localhost:3000',
-        basePath: BASE_API_PATH,
-        produces: [
-            "application/json"
-        ],
-        schemes: ['http', 'https'],
-		securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
-    },
-    basedir: __dirname, //app absolute path
-    files: ['*.js'] //Path to the API handle folder
-};
+const database = require("../database");
 
 
-console.log("Starting API server...")
+/**
+ * @typedef Delivery
+ * @property {integer} _id           - UUID
+ * @property {integer} _providerId   - Identifier
+ * @property {integer} _userId      - Identifier
+ * @property {string}  products   - products
+ * @property {integer}     state  - state
+ * @property {string}  paid        - paid
+ * @property {string}  createDate    - createDate
+ */
 
-app.use(bodyParser.json());
+/**
+ * @typedef DeliveryError
+ * @property {string} todo.required - TODO
+ */
 
-var db = new dataStore({
-    filename: DB_FILE_NAME,
-    autoload: true
-})
 
-app.get("/", (req, res) => {
-    res.send ("<html><body><h1>My server</h1></body></html>");
-});
-
-app.get(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + "- GET /contacts");
-    db.find({}, (err,contacts) => {
-        if (err) {
-            console.log(Date() + " - " + err);
-            res.sendStatus(500);
-        } else {
-            res.send(contacts.map((contact) => {
-                delete contact._id;
-                return contact;
-            }));
-        }
-    })
-});
-
-app.post(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + "- POST /contacts");
-    var contact = req.body;
-    db.insert(contact, (err) => {
-        if (err) {
-            console.log(Date() + " - " + err);
-            res.sendStatus(500);
-        } else {
-            res.sendStatus(201);
-        }
-    });
-    
-});
 
 /**
  * Get all deliveries if empty, or selected delivery by _id
@@ -105,31 +49,6 @@ const getMethod = (req, res) => {
       });
     }
   };
-
-/**
- * @typedef format
- * @property {string}  type
- * @property {integer}     price
- *
- */
-
-
-/**
- * @typedef Delivery
- * @property {integer} _id           - UUID
- * @property {integer} _providerId   - Identifier
- * @property {integer} _userId      - Identifier
- * @property {string}  products   - products
- * @property {integer}     state  - state
- * @property {string}  paid        - paid
- * @property {string}  createDate    - createDate
- */
-
-/**
- * @typedef DeliveryError
- * @property {string} todo.required - TODO
- */
-
 
 /**
  * Create a new deliveries when a payment is generated
@@ -226,44 +145,3 @@ module.exports.register = (apiPrefix, router) => {
   router.put(apiPrefix + "/deliveries", putMethod);
   router.delete(apiPrefix + "/deliveries", deleteMethod);
 };
-
-/**
- * Get all returns if empty, or selected return by _id
- * @route GET /returns
- * @group Returns - Returns
- * @param {string} returnId.query -  If empty returns all returns
- * @returns {Returns} 200 - Returns wheter selected return or all returns
- * @returns {ReturnsError} default - unexpected error
- */
-
-/**
- * Create a new return when is request for user
- * @route POST /returns
- * @group Returns - Returns
- * @param {Returns} returns.body.required - New return
- * @returns {integer} 200 - Returns the  created return
- * @returns {ReturnError} default - unexpected error
- */
-
- /**
- * Update an existing return
- * @route PUT /returns
- * @group Returns - Returns
- * @param {string} returnId.query.required -  Return Id
- * @param {Return.model} return.body.required - New value for the return
- * @returns {Return} 200 - Returns the current state for this returns
- * @returns {ReturnError} default - unexpected error
- */
-
- /**
- * Delete an existing return
- * @route DELETE /returns
- * @group Returns - Returns
- * @param {string} returnId.query.required -  Return Id
- * @returns {Return} 200 - Returns the current state for this returns
- * @returns {ReturnError} default - unexpected error
- */
-
-expressSwagger(options);
-app.listen(port);
-console.log("Server ready!");
