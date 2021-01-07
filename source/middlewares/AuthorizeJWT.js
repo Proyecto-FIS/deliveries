907eda34dc4801d7422bad10612f2a53160994ed
danmarrod <danmarrod@alum.us.es>
2021-01-07 21:01:42 +0100

    Added authentication with CircuitBreaker

--- source/middlewares/AuthorizeJWT.js
+++ source/middlewares/AuthorizeJWT.js
@@ -1,39 +1,52 @@
 const axios = require("axios");
 const mongoose = require("mongoose");
+const createCircuitBreaker = require("../CircuitBreaker").createCircuitBreaker;
 
 /**
  * @typedef UserAuthError
  * @property {string} reason    - User-friendly reason message
  */
 
+const usersAuthService = createCircuitBreaker({
+  name: "usersAuthService",
+  errorThreshold: 20,
+  timeout: 8000,
+  healthRequests: 5,
+  sleepTimeMS: 100,
+  maxRequests: 0,
+  errorHandler: (err) => false,
+  request: (token) => axios.get(`${process.env.USERS_MS}/auth/${token}`),
+  fallback: (err, args) => {
+      if(err && err.isAxiosError) throw err;
+      throw ({
+          response: {
+              status: 503
+          }
+      });
+  }
+});
+
+
 const AuthorizeJWT = (req, res, next) => {
   const token = req.body.userToken || req.query.userToken;
-  axios
-    .get(`${process.env.USERS_MS}/auth/${token}`)
-    .then((response) => {
-      const userID = mongoose.Types.ObjectId(response.data.account_id);
-      const isCustomer = response.data.isCustomer;
-      if (isCustomer)
-        throw {response:{status: 403}}
-      if (req.body.userToken) {
-        req.body.userID = userID;
-      } else {
-        req.query.userID = userID;
-      }
-      next();
-    })
-    .catch((err) => {
-      if(err.response.status === 403){
-        res.status(403).json({reason: "Customers cannot operate this API"})
-      } 
-      else if (err.response.status === 500) {
-        res.status(401).json({ reason: "Authentication failed" });
-      } else {
-        res
-          .status(500)
-          .json({ reason: "Users service temporarily unavailable" });
-      }
-    });
-};
+
+  usersAuthService.execute(token)
+      .then(response => {
+          const userID = mongoose.Types.ObjectId(response.data.account_id);
+          if (req.body.userToken) {
+              req.body.userID = userID;
+          } else {
+              req.query.userID = userID;
+          }
+          next();
+      })
+      .catch(err => {
+          if (err.response.status === 500 || err.response.status === 401) {
+              res.status(401).json({ reason: "Authentication failed" });
+          } else {
+              res.status(err.response.status).json({ reason: "Users service is down" });
+          }
+      });
+}
 
 module.exports = AuthorizeJWT;
