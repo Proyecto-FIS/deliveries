14c9ab78cb51645ccfb99576caefe693950adf28
danmarrod <danmarrod@alum.us.es>
2021-01-27 23:43:52 +0100

    added encapsuled command for CB in users services

--- source/middlewares/AuthorizeJWT.js
+++ source/middlewares/AuthorizeJWT.js
@@ -1,16 +1,36 @@
 const axios = require("axios");
 const mongoose = require("mongoose");
+const createCircuitBreaker = require("../CircuitBreaker").createCircuitBreaker;
 
 /**
  * @typedef UserAuthError
  * @property {string} reason    - User-friendly reason message
  */
 
+const usersAuthService = createCircuitBreaker({
+  name: "usersAuthService",
+  errorThreshold: 20,
+  timeout: 8000,
+  healthRequests: 5,
+  sleepTimeMS: 100,
+  maxRequests: 0,
+  errorHandler: (err) => false,
+  request: (token) => axios.get(`${process.env.USERS_MS}/auth/${token}`),
+  fallback: (err, args) => {
+      if(err && err.isAxiosError) throw err;
+      throw ({
+          response: {
+              status: 503
+          }
+      });
+  }
+});
+
 const AuthorizeJWT = (req, res, next) => {
   const token = req.body.userToken || req.query.userToken;
   console.log("Authorization processing, token=" + token);
-  axios
-    .get(`${process.env.USERS_MS}/auth/${token}`)
+
+  usersAuthService.execute(token)
     .then((response) => {
       const userId = mongoose.Types.ObjectId(response.data.account_id);
       if (req.body.userToken) {
